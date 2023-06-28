55688ff5c2eb7e79aec842bd9810d3d70e832a83
Gaven Tran <gaven.q.tran@gmail.com>
2023-06-28 19:04:05 -0400

    Reevaluate event listener's functions
    At this point, the event listeners for the digits, operations,
    and final buttons work as intended! But, they only with single digit
    operands and only one operation can occur unless you use the "CLEAR"
    button. This is the most basic form of the calculator so far!

--- scripts.js
+++ scripts.js
@@ -41,15 +41,19 @@ digitButtons.forEach(button => button.addEventListener("click", () => {
     if(operand1 === undefined) {
         operand1 = button.textContent;
         console.log(operand1);
-    } else if (operand2 === undefined) {
+    } else if ((operand2 === undefined) && (operation)) {
         operand2 = button.textContent;
         console.log(operand2);
     }
+
     /* This section works, but only with single digit operands.
         * To make it so operand1 can be multiple numbers, keep assigning operand1 numbers (probs as a string) until an operation is chosen
         * To make it so operand2 can be multiple numbers, keep assigning operand2 numbers until the equal sign is chosen.
     */
-    /* Add part here where it add the text to the output section */
+
+    /* 
+    Add part here where it add the text to the output section 
+    */
 }));
 operationButtons.forEach(button => button.addEventListener("click", () => {
     if(!operation) {
@@ -63,8 +67,10 @@ operationButtons.forEach(button => button.addEventListener("click", () => {
 finalButtons.forEach(button => button.addEventListener("click", () => {
     switch (button.textContent) {
         case "=":
-            finalValue = operate(operand1, operation, operand2);
-            console.log(finalValue);
+            if((operand1 || operand1 === 0) && operation && (operand2 || operand2 === 0)) {
+                finalValue = operate(operand1, operation, operand2);
+                console.log(finalValue);  
+            }
             /* Code that displays it to the outcome-text section */
             /* Code that makes it so this value then also be used in the next operation if the user decided to add another operation. Apply finalValue to operand1? */
             break;
@@ -72,7 +78,7 @@ finalButtons.forEach(button => button.addEventListener("click", () => {
             operand1 === undefined;
             operation === undefined;
             operand2 === undefined;
-            /* Code that clears out the test to the output section, maybe just leave it as a space w/ " "? */
+            /* Code that clears out the text to the output section, maybe just leave it as a space w/ " "? */
             break;
     }
 }));

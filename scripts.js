9e2b9c4f2251a461b12119689ad7e25a1091a91a
Gaven Tran <gaven.q.tran@gmail.com>
2023-06-28 20:13:06 -0400

    Display operands/operation to output text

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-/* Technical things first, Presentation last */
 function add(a, b) {
     return a + b;
 }
@@ -29,23 +28,19 @@ const digitButtons = document.querySelectorAll(".digit-button");
 const operationButtons = document.querySelectorAll(".operation-button");
 const finalButtons = document.querySelectorAll(".final-button");
 
+const outputText = document.querySelector("#output-text");
+
 let operand1; let operation; let operand2;
 let preOperand1 = ""; let preOperand2 = "";
 
 digitButtons.forEach(button => button.addEventListener("click", () => {
     if(operand1 === undefined) {
         preOperand1 += button.textContent;
-        console.log(operand1); /* not valid */
+        outputText.textContent += button.textContent;
     } else if ((operand2 === undefined) && operation && (operand1 || operand1 === 0)) {
         preOperand2 += button.textContent;
-        console.log(operand2);
+        outputText.textContent += button.textContent;
     }
-
-    /* This section works, but only with single digit operands.
-        * To make it so operand1 can be multiple numbers, keep assigning operand1 numbers (probs as a string) until an operation is chosen
-        * To make it so operand2 can be multiple numbers, keep assigning operand2 numbers until the equal sign is chosen.
-    */
-
     /* 
     Add part here where it add the text to the output section 
     */
@@ -53,12 +48,11 @@ digitButtons.forEach(button => button.addEventListener("click", () => {
 operationButtons.forEach(button => button.addEventListener("click", () => {
     if(!operation) {
         operation = button.textContent;
+        outputText.textContent += " " + button.textContent + " ";
         if (!operand1) {
             operand1 = preOperand1;  
         }
-        console.log(operation);
     }
-    /* It's good to note that the operation symbols all come out as true */
     /* Add part here where it adds the text to the output section */
 }));
 
@@ -68,19 +62,17 @@ finalButtons.forEach(button => button.addEventListener("click", () => {
             operand2 = preOperand2;
             if((operand1 || operand1 === 0) && operation && (operand2 || operand2 === 0)) {
                 solution = operate(operand1, operation, operand2);
-                console.log(solution);
                 operand1 = solution; 
                 operand2 = undefined; operation = undefined;
                 preOperand1 = ""; preOperand2 = "";
             }
-            /* Code that displays it to the outcome-text section */
-
+            outputText.textContent = solution;
             break;
         case "CLEAR":
             operand1 = undefined;
             operation = undefined;
             operand2 = undefined;
-            /* Code that clears out the text to the output section, maybe just leave it as a space w/ " "? */
+            outputText.textContent = "";
             break;
     }
 }));
